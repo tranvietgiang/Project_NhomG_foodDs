d007a9ef3fabcd9cd701aabcd75189a9ad204b78
tranvietgiang <tranvietgiang25@gmail.com>
2025-03-27 15:08:21 +0700

    complete search client, di chuyển qua lại các page

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('phone')->nullable();
+            $table->string('phone')->nullable(true);
             $table->string('role')->default('user');
             $table->string('last_activity')->default('off');
             $table->rememberToken();
