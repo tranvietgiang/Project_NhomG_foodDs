87bb27407e9db044d1cfd0abeea8b2256b71bdf6
wedbanhang <capham18@gmail.com>
2025-05-19 16:15:42 +0700

    CRUD giỏ hàng

--- Project_NhomG_foodDs/resources/views/component/belowContent/cart.blade.php
+++ Project_NhomG_foodDs/resources/views/component/belowContent/cart.blade.php
@@ -23,17 +23,21 @@
                 </tr>
             </thead>
             <tbody>
-
+                <!-- giỏ  Hàng -->
+                <input type="hidden">
                 <!-- Giả sử bạn sẽ thêm sản phẩm vào đây bằng PHP -->
                 @foreach($cartItems as $item)
                 <tr>
                     <td><img src="{{ asset('images/'. $item->product_image) }}" alt=""></td>
                     <td>{{$item->product_name}}</td>
                     <td>
-                        <form action="{{ route('cart.update', $item->product_id) }}" method="POST" style="display:inline;">
+                        <form action="{{ route('cart.update', $item->product_id) }}" method="POST"
+                            style="display:inline;">
                             @csrf
-                            @method('PUT') <!-- Xác định yêu cầu PUT cho cập nhật -->
-                            <input type="number" name="quantity" value="{{$item->quantity_sp}}" min="1" class="form-control" onchange="this.form.submit()" />
+                            @method('PUT')
+                            <!-- Xác định yêu cầu PUT cho cập nhật -->
+                            <input type="number" name="quantity" value="{{$item->quantity_sp}}" min="1"
+                                class="form-control" onchange="this.form.submit()" />
                         </form>
                     </td>
                     <td>{{ number_format($item->product_price, 0, ',', '.') }} VND</td>
@@ -41,9 +45,11 @@
                         {{ number_format($item->product_price * $item->quantity_sp, 0, ',', '.') }} VND
                     </td>
                     <td>
-                        <form action="{{ route('cart.removeCart', $item->product_id) }}" method="POST" style="display:inline;">
+                        <form action="{{ route('cart.removeCart', $item->product_id) }}" method="POST"
+                            style="display:inline;">
                             @csrf
-                            @method('DELETE') <!-- Yêu cầu DELETE cho xóa sản phẩm -->
+                            @method('DELETE')
+                            <!-- Yêu cầu DELETE cho xóa sản phẩm -->
                             <button class="btn btn-danger">Xóa</button>
                         </form>
                     </td>
@@ -57,13 +63,13 @@
         <button class="btn btn-primary">Tiến Hành Thanh Toán</button>
         <div class="mt-2">
             <form action="{{ route('momo_payment') }}" method="post">
-        @csrf
-            <input type="hidden" name="totalAmount" value="{{$totalAmount}}">
-             <button class="momo btn btn-primary">Thanh toán  bằng  momo </button>
-        </form>
+                @csrf
+                <input type="hidden" name="totalAmount" value="{{$totalAmount}}">
+                <button class="momo btn btn-primary">Thanh toán bằng momo </button>
+            </form>
         </div>
-        
-       
+
+
     </div>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
